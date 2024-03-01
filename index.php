39bbadcd0406b25ca5cf2d403b720ecf54c3e5c5
sarahelshark <sarahelsh183@gmail.com>
2024-03-01 21:40:42 +0100

    test push

--- index.php
+++ index.php
@@ -126,6 +126,7 @@ foreach($boolean_members as $key => $member) {
 }
 
 /* Snack 7
+ll
 Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.*/
 
 $classe = [
